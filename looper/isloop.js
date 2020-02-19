de322dcce9a648ff049c491ae988656d635b9b97
Vosters <mike.r.vosters@gmail.com>
2020-02-19 16:00:28 -0500

    Completed isloop algo

--- looper/isloop.js
+++ looper/isloop.js
@@ -2,7 +2,23 @@
 
 //Complete this algo
 const isLoop = (linkedlist) => {
+    // console.log('LINKED LIST', linkedlist)
+    // console.log('NODE EQUALS', linkedlist.head === linkedlist.head)
 
+    let head = linkedlist.head;
+    let arr = []
+
+    while (head.next) {
+        for (let i = 0; i < arr.length; i++) {
+            if (arr[i] === head) {
+                return true
+            }
+        }
+        arr.push(head);
+        head = head.next
+    }
+
+    return false
 };
 
 
